4b3deb8a62ef0e406aa5367a165e239cf4ea3ff1
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-01-25 23:29:25 +0100

    feat(tailwind): improve structure and styles
    Description:
    - Refactor the code by applying Tailwind's CSS utility classes to improve the visual appearance through responsive and stylized styles to the `input`, `label` and `p` tags.
    These changes improve the user interface by making form `inputs` more visually appealing and easier to use.
    
    References:
    - https://flowbite.com/docs/forms/input-field/
    - https://flowbite.com/docs/components/card/

--- src/views/exercises/1-begin.vue
+++ src/views/exercises/1-begin.vue
@@ -6,15 +6,26 @@ const item = ref("coffee");
 <template>
   <div class="viewport-center">
     <div>
-      <label for="name">Name:</label>
-      <input type="text" name="name" id="name" v-model="name">
-      <br/>
-      <label for="item">Item:</label>
-      <input type="text" name="item" id="item" v-model="item">
-      <hr/>
-      <p>
-        Hello {{ name }}! Do you like {{ item }}?
-      </p>
+      <div class="mb-4">
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Name:
+        </label>
+        <input type="text" name="name" id="name" v-model="name" placeholder="Enter your name" required
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      </div>
+      <div>
+        <label for="item" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Item:
+        </label>
+        <input type="text" name="item" id="item" v-model="item" placeholder="Enter an item" required
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      </div>
+      <div
+        class="block w-full mt-8 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <p>
+          Hello {{ name }}! Do you like {{ item }}?
+        </p>
+      </div>
     </div>
   </div>
 </template>
